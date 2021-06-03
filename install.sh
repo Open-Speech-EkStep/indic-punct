1f5abc6c429ea53bbe5daf350f39e767081fa49b
Anirudh Gupta <anirudh@pop-os.localdomain>
2021-06-03 13:03:32 +0530

    Anirudh | adding punctuation for English

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt-get update && apt-get install -y libsndfile1 ffmpeg
+pip install Cython
+pip install nemo_toolkit['all']==1.0.0rc1
+
+conda install -c conda-forge pynini==2.1.4
--- src/punctuate/punctuate_text.py
+++ src/punctuate/punctuate_text.py
@@ -7,15 +7,20 @@ from indicnlp.tokenize import indic_tokenize
 import os
 import wget
 import sys
+from nemo.collections.nlp.models import PunctuationCapitalizationModel
 
 
 class Punctuation:
     def __init__(self, language_code):
         self.language_code = language_code
-        self.model_path = 'model_data/' + self.language_code + '.pt'
-        self.encoder_path = 'model_data/' + self.language_code + '.json'
-        self.dict_map = 'model_data/' + self.language_code + '_dict.json'
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        if self.language_code == 'en':
+            self.model = PunctuationCapitalizationModel.from_pretrained("punctuation_en_bert")
+        else:
+            self.model_path = 'model_data/' + self.language_code + '.pt'
+            self.encoder_path = 'model_data/' + self.language_code + '.json'
+            self.dict_map = 'model_data/' + self.language_code + '_dict.json'
+            self.device = "cuda" if torch.cuda.is_available() else "cpu"
+            self.tokenizer, self.model = self.load_model_parameters()
 
 
     def bar_thermometer(self, current, total, width=80):
@@ -64,48 +69,58 @@ class Punctuation:
 
     def get_tokens_and_labels_indices_from_text(self, text):
 
-        tokenizer, model = self.load_model_parameters()
-
-        tokenized_sentence = tokenizer.encode(text)
+        tokenized_sentence = self.tokenizer.encode(text)
         input_ids = torch.tensor([tokenized_sentence]).to(self.device)
         with torch.no_grad():
-            output = model(input_ids)
+            output = self.model(input_ids)
         label_indices = np.argmax(output[0].to('cpu').numpy(), axis=2)
-        tokens = tokenizer.convert_ids_to_tokens(input_ids.to('cpu').numpy()[0])
+        tokens = self.tokenizer.convert_ids_to_tokens(input_ids.to('cpu').numpy()[0])
         return tokens, label_indices
 
-    def punctuate_text(self, text):
-        tokens, label_indices = self.get_tokens_and_labels_indices_from_text(text)
+    def punctuate_text_hindi(self, text):
+
         with open(self.encoder_path) as label_encoder:
             train_encoder = json.load(label_encoder)
 
         with open(self.dict_map) as dict_map:
             punctuation_dict = json.load(dict_map)
 
-        new_tokens = []
-        new_labels = []
-        for i in range(1, len(tokens) - 1):
-            if tokens[i].startswith("▁"):
-                current_word = tokens[i][1:]
-                new_labels.append(list(train_encoder.keys())[list(train_encoder.values()).index(label_indices[0][i])])
-                for j in range(i + 1, len(tokens) - 1):
-                    if not tokens[j].startswith("▁"):
-                        current_word = current_word + tokens[j]
-                    if tokens[j].startswith("▁"):
-                        break
-                new_tokens.append(current_word)
-        full_text = ''
-        tokenized_text = indic_tokenize.trivial_tokenize_indic(text)
+        punctuated_sentences = []
 
-        if len(tokenized_text) == len(new_labels):
-            full_text_tokens = tokenized_text
-        else:
-            full_text_tokens = new_tokens
+        for sentence in text:
 
-        for word, punctuation in zip(full_text_tokens, new_labels):
-            full_text = full_text + word + punctuation_dict[punctuation]
-        return full_text
+            tokens, label_indices = self.get_tokens_and_labels_indices_from_text(sentence)
+
+            new_tokens = []
+            new_labels = []
+            for i in range(1, len(tokens) - 1):
+                if tokens[i].startswith("▁"):
+                    current_word = tokens[i][1:]
+                    new_labels.append(list(train_encoder.keys())[list(train_encoder.values()).index(label_indices[0][i])])
+                    for j in range(i + 1, len(tokens) - 1):
+                        if not tokens[j].startswith("▁"):
+                            current_word = current_word + tokens[j]
+                        if tokens[j].startswith("▁"):
+                            break
+                    new_tokens.append(current_word)
+            full_text = ''
+            tokenized_text = indic_tokenize.trivial_tokenize_indic(sentence)
+
+            if len(tokenized_text) == len(new_labels):
+                full_text_tokens = tokenized_text
+            else:
+                full_text_tokens = new_tokens
+
+            for word, punctuation in zip(full_text_tokens, new_labels):
+                full_text = full_text + word + punctuation_dict[punctuation]
+            punctuated_sentences.append(full_text)
+
+        return punctuated_sentences
+
+    def punctuate_text_english(self, text):
+        return self.model.add_punctuation_capitalization(text)
 
 
 if __name__ == "__main__":
-    print(Punctuation('hi').punctuate_text('नीरव मोदी को लंदन में पकड़ लिया गया था लेकिन मेहुल चोकसी लगातार एंटीगुआ में छिपा हुआ था मेहुल को भारत को सौंप दिया जाए'))
+    print(Punctuation('hi').punctuate_text_hindi(['नीरव मोदी को लंदन में पकड़ लिया गया था लेकिन मेहुल चोकसी लगातार एंटीगुआ में छिपा हुआ था', ' मेहुल को भारत को सौंप दिया जाए']))
+    print(Punctuation('en').punctuate_text_english(['how are you', 'great how about you']))
