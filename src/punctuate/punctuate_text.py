8e8f69046f59907037f0b2a71a9f01c53f2a8486
Ankur Dhuriya <mail.ankurdhuriya@gmail.com>
2021-10-18 12:31:13 +0530

    marathi model added

--- src/punctuate/punctuate_text.py
+++ src/punctuate/punctuate_text.py
@@ -125,7 +125,7 @@ class Punctuation:
     def punctuate_text(self, text):
         if self.language_code == 'en':
             return self.punctuate_text_english(text)
-        elif self.language_code in ['hi', 'gu', 'te']:
+        elif self.language_code in ['hi', 'gu', 'te', 'mr]:
             return self.punctuate_text_others(text)
 
 
