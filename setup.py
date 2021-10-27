872ac46e15bbc02afa529f209721e3f5488c94e7
neerajchhimwal <neerajchhimwal21197@gmail.com>
2021-10-27 11:58:49 +0530

    Updating version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name="indic-punct",
-    version='0.0.6',
+    version='0.0.7',
     description='Punctuation and inverse text normalization for Indic languages and English',
     long_description=long_description,
     long_description_content_type='text/markdown',
