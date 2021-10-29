20fa53f71bf8f1adef13f07e4499b135ff03bcc2
Priyanshi-Shah <41450194+Priyanshi-Shah@users.noreply.github.com>
2021-10-29 11:37:43 +0530

    Update punctuate_text.py

--- src/punctuate/punctuate_text.py
+++ src/punctuate/punctuate_text.py
@@ -140,7 +140,7 @@ class Punctuation:
     def punctuate_text(self, text):
         if self.language_code == 'en':
             return self.punctuate_text_english(text)
-        elif self.language_code in ['hi', 'gu', 'te', 'mr', 'kn', 'pa']:
+        elif self.language_code in ['hi', 'gu', 'te', 'mr', 'kn', 'pa','ta']:
             return self.punctuate_text_others(text)
 
 
