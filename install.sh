19855f465ba3b9515e520c62113c063bf5aa33f5
Anirudh Gupta <anirudhgupta12@gmail.com>
2021-11-02 12:14:55 +0530

    Anirudh | changing pip tp pip3

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update && apt-get install -y libsndfile1 ffmpeg
 pip install Cython
 conda install libgcc gmp
-pip install nemo_toolkit['all']==v1.0.2
+pip3 install nemo_toolkit['all']==v1.0.2
 
 conda install -c conda-forge pynini==2.1.4
