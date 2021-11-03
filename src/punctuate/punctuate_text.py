8e27e4737e4cfbfd57109ae9d37e9dea0ec44e70
Priyanshi-Shah <41450194+Priyanshi-Shah@users.noreply.github.com>
2021-11-03 15:14:28 +0530

    Updating for Odia and Malayalam

--- src/punctuate/punctuate_text.py
+++ src/punctuate/punctuate_text.py
@@ -140,7 +140,7 @@ class Punctuation:
     def punctuate_text(self, text):
         if self.language_code == 'en':
             return self.punctuate_text_english(text)
-        elif self.language_code in ['hi', 'gu', 'te', 'mr', 'kn', 'pa', 'ta', 'bn']:
+        elif self.language_code in ['hi', 'gu', 'te', 'mr', 'kn', 'pa', 'ta', 'bn', 'or', 'ml']:
             return self.punctuate_text_others(text)
 
 
